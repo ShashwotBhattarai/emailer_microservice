d93adfae68cd452afea6c1c82c2971b5d9d73940
ShashwotBhattarai <shashwot.tech@gmail.com>
2023-10-17 01:13:29 +0545

    emailer working

--- index.ts
+++ index.ts
@@ -1,26 +1,27 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  port: 465,               // true for 465, false for other ports
-  host: "smtp.gmail.com",
-     auth: {
-          user: 'shashwot.media@gmail.com',
-          pass: 'f966LMyyYYHXnT4',
-       },
-  secure: true,
+async function sendMail() {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "shashwot.media@gmail.com",
+      pass: "igfi bakt gsgh graj",
+    },
   });
 
-var mailOptions = {
-  from: 'shashwot.media@gmail.com',  // sender address
-  to: 'babudallay@gmail.com',   // list of receivers
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
+  const mailOptions = {
+    from: "shashwot.media@gmail.com",
+    to: "babudallay@gmail.com,acstockthankot@gmail.com",
+    subject: "Welcome to NodeJS App",
+    text: "This is an email using nodemail in nodejs, chalyo re chalyo",
+  };
 
-transporter.sendMail(mailOptions, function(error:any, info:any){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
+  try {
+    const result = await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.log("Email send failed with error:", error);
   }
-}); 
+}
+
+sendMail();
